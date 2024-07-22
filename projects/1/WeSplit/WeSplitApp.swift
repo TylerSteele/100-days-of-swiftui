33852770ad66d956cb75fc4febcff806f73e0ad3
SteeleTyler <tylermsteele@outlook.com>
2024-07-22 09:35:59 -0400

    Complete day 17

---
+++ projects/1/WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Tyler Steele on 7/21/24.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
