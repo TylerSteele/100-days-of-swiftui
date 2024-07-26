df3a5f854e7b9c84cad2d6751fcbf3ca7b82dea6
SteeleTyler <tylermsteele@outlook.com>
2024-07-26 09:20:56 -0400

    Complete day 21

---
+++ projects/2/GuessThatFlag/GuessThatFlag/GuessThatFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessThatFlagApp.swift
+//  GuessThatFlag
+//
+//  Created by Tyler Steele on 7/25/24.
+//
+
+import SwiftUI
+
+@main
+struct GuessThatFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
