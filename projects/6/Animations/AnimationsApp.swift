457061bd7c8f0254103f20c5d2f6cb32ca298981
SteeleTyler <tylermsteele@outlook.com>
2024-08-08 09:34:10 -0400

    Complete day 32

---
+++ projects/6/Animations/AnimationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationsApp.swift
+//  Animations
+//
+//  Created by Tyler Steele on 8/8/24.
+//
+
+import SwiftUI
+
+@main
+struct AnimationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ projects/6/Animations/ContentView.swift
@@ -0,0 +1,28 @@
+//
+//  ContentView.swift
+//  Animations
+//
+//  Created by Tyler Steele on 8/8/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var animationAmount = 0.0
+    var body: some View {
+        Button("Tap Me") {
+            withAnimation(.spring(duration: 1, bounce: 0.5)) {
+                animationAmount += 360
+            }
+        }
+        .padding(50)
+        .background(.red)
+        .foregroundStyle(.white)
+        .clipShape(.circle)
+        .rotation3DEffect(.degrees(animationAmount), axis: (x: 1, y: 1, z: 1))
+    }
+}
+
+#Preview {
+    ContentView()
+}
