1646a6e78041c7a56f59422784e15c69d8a3ea92
SteeleTyler <tylermsteele@outlook.com>
2024-08-09 09:40:29 -0400

    Complete day 33

--- projects/6/Animations/ContentView.swift
+++ projects/6/Animations/ContentView.swift
@@ -7,21 +7,52 @@
 
 import SwiftUI
 
-struct ContentView: View {
-    @State private var animationAmount = 0.0
-    var body: some View {
-        Button("Tap Me") {
-            withAnimation(.spring(duration: 1, bounce: 0.5)) {
-                animationAmount += 360
-            }
-        }
-        .padding(50)
-        .background(.red)
-        .foregroundStyle(.white)
-        .clipShape(.circle)
-        .rotation3DEffect(.degrees(animationAmount), axis: (x: 1, y: 1, z: 1))
+struct CornerRotateModifier: ViewModifier {
+    let amount: Double
+    let anchor: UnitPoint
+    
+    func body(content: Content) -> some View {
+        content
+            .rotationEffect(.degrees(amount), anchor: anchor)
+            .clipped()
     }
 }
+
+extension AnyTransition {
+    static var pivot: AnyTransition {
+        .modifier(
+            active: CornerRotateModifier(amount: -90, anchor: .topLeading),
+            identity: CornerRotateModifier(amount: 0, anchor: .topLeading)
+        )
+    }
+}
+
+struct ContentView: View {
+    @State private var isShowingRed = false
+
+    var body: some View {
+        ZStack {
+            if isShowingRed {
+                Rectangle()
+                    .fill(.red)
+                    .frame(width: 200, height: 200)
+                    .transition(.pivot)
+            } else {
+                Rectangle()
+                    .fill(.blue)
+                    .frame(width: 200, height: 200)
+                    .transition(.pivot)
+            }
+        }
+        .onTapGesture {
+            withAnimation {
+                isShowingRed.toggle()
+            }
+        }
+    }
+}
+
+
 
 #Preview {
     ContentView()
