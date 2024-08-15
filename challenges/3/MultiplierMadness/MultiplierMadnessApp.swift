5f51dcc1851d7dacc6361d166b8dc03afccbcfd4
SteeleTyler <tylermsteele@outlook.com>
2024-08-15 08:52:16 -0400

    Start challeng 3

---
+++ challenges/3/MultiplierMadness/MultiplierMadnessApp.swift
@@ -0,0 +1,17 @@
+//
+//  MultiplierMadnessApp.swift
+//  MultiplierMadness
+//
+//  Created by Tyler Steele on 8/14/24.
+//
+
+import SwiftUI
+
+@main
+struct MultiplierMadnessApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
