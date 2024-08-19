65a991aa61b00b00aea3ff2e5db98a8a869202c5
SteeleTyler <tylermsteele@outlook.com>
2024-08-19 10:10:13 -0400

    Complete day 41

---
+++ projects/8/Moonshot/MoonshotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonshotApp.swift
+//  Moonshot
+//
+//  Created by Tyler Steele on 8/16/24.
+//
+
+import SwiftUI
+
+@main
+struct MoonshotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ projects/8/Moonshot/astronaut.swift
@@ -0,0 +1,14 @@
+//
+//  astronaut.swift
+//  Moonshot
+//
+//  Created by Tyler Steele on 8/18/24.
+//
+
+import Foundation
+
+struct Astronaut: Codable, Identifiable {
+    let id: String
+    let name: String
+    let description: String
+}
