a4cdf366a67314511475fc27f184f1eb6ff982a9
SteeleTyler <tylermsteele@outlook.com>
2024-08-29 23:34:35 -0400

    Complete day 51

---
+++ projects/10/CheckoutView.swift
@@ -0,0 +1,72 @@
+//
+//  CheckoutView.swift
+//  CupcakeCorner
+//
+//  Created by Tyler Steele on 8/29/24.
+//
+
+import SwiftUI
+
+struct CheckoutView: View {
+    var order: Order
+    
+    @State private var confirmationMessage = ""
+    @State private var showConfirmation = false
+    
+    var body: some View {
+        ScrollView {
+            VStack {
+                AsyncImage(url: URL(string: "https://hws.dev/img/cupcakes@3x.jpg"),
+                           scale: 3) { image in
+                    image
+                        .resizable()
+                        .scaledToFit()
+                } placeholder: {
+                    ProgressView()
+                }
+                .frame(height: 233)
+                
+                Text("Your total cost is \(order.cost, format: .currency(code: "USD"))")
+                    .font(.title)
+                
+                Button("Place Order", action: {
+                    Task {
+                        await placeOrder()
+                    }
+                })
+                .padding()
+            }
+        }
+        .navigationTitle("Check out")
+        .navigationBarTitleDisplayMode(.inline)
+        .scrollBounceBehavior(.basedOnSize)
+        .alert("Thank you!", isPresented: $showConfirmation) { }
+        message : {
+            Text(confirmationMessage)
+        }    }
+    
+    func placeOrder() async {
+        guard let encoded = try? JSONEncoder().encode(order) else {
+            print("Failed to encode order")
+            return
+        }
+        
+        let url = URL(string: "https://reqres.in/api/cupcakes")!
+        var request = URLRequest(url: url)
+        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        request.httpMethod = "POST"
+        
+        do {
+            let (data, _) = try await URLSession.shared.upload(for: request, from: encoded)
+            
+            let decodedOrder = try JSONDecoder().decode(Order.self, from: data)
+            confirmationMessage = "Your order for \(decodedOrder.quantity)x \(Order.types[decodedOrder.type].lowercased()) is on its way!"
+        } catch {
+            print("Check out failed \(error.localizedDescription)")
+        }
+    }
+}
+
+#Preview {
+    CheckoutView(order: Order())
+}
