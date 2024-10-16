06b84a4b352700abc12cce3e6b4395661860e3eb
SteeleTyler <tylermsteele@outlook.com>
2024-10-15 22:54:44 -0400

    Complete day 67

---
+++ projects/13/Instafilter/InstafilterApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstafilterApp.swift
+//  Instafilter
+//
+//  Created by Tyler Steele on 10/7/24.
+//
+
+import SwiftUI
+
+@main
+struct InstafilterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
