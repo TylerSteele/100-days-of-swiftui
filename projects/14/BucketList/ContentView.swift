1167c639cdfaecf6dfb1e132bf0ecb84767cb3f8
SteeleTyler <tylermsteele@outlook.com>
2024-10-21 23:34:24 -0400

    Complete day 70

---
+++ projects/14/BucketList/ContentView.swift
@@ -0,0 +1,53 @@
+//
+//  ContentView.swift
+//  BucketList
+//
+//  Created by Tyler Steele on 10/16/24.
+//
+
+import MapKit
+import SwiftUI
+
+struct ContentView: View {
+    let startPosition = MapCameraPosition.region(MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 56, longitude: -3), span: MKCoordinateSpan(latitudeDelta: 10, longitudeDelta: 10)))
+    
+    @State private var locations = [Location]()
+    @State private var selectedPlace: Location?
+    
+    var body: some View {
+        MapReader { proxy in
+            Map(initialPosition: startPosition) {
+                ForEach(locations) { location in
+                    Annotation(location.name, coordinate: location.coordinate) {
+                        Image(systemName: "star.circle")
+                            .resizable()
+                            .foregroundStyle(.purple)
+                            .frame(width: 44, height: 44)
+                            .background(.white)
+                            .clipShape(.circle)
+                            .onLongPressGesture {
+                                selectedPlace = location
+                            }
+                    }
+                }
+            }
+            .onTapGesture { position in
+                if let coordinate = proxy.convert(position, from: .local) {
+                    locations.append(Location(id: UUID(), name: "New location", description: "", latitude: coordinate.latitude, longitude: coordinate.longitude))
+                }
+            }
+            .sheet(item: $selectedPlace) { place in
+                EditView(location: place) { newLocation in
+                    if let index = locations.firstIndex(of: place) {
+                        locations[index] = newLocation
+                    }
+                }
+            }
+            
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
