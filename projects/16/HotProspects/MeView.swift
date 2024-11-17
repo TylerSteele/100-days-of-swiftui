64d7aa4b55d027d5e5c9679d8fd39729d70ea280
SteeleTyler <tylermsteele@outlook.com>
2024-11-17 13:54:42 -0500

    Complete day 84

--- projects/16/HotProspects/MeView.swift
+++ projects/16/HotProspects/MeView.swift
@@ -11,6 +11,9 @@ import SwiftUI
 struct MeView: View {
     @AppStorage("name") private var name = "Anonymous"
     @AppStorage("emailAddress") private var emailAddress = "you@yoursite.com"
+    @State private var qrCode = UIImage()
+    
+    
     
     let context = CIContext()
     let filter = CIFilter.qrCodeGenerator()
@@ -25,14 +28,24 @@ struct MeView: View {
                     .textContentType(.emailAddress)
                     .font(.title)
                 
-                Image(uiImage: generateQRCode(from: "\(name)\n\(emailAddress)"))
+                Image(uiImage: qrCode)
                     .interpolation(.none)
                     .resizable()
                     .scaledToFit()
                     .frame(width: 200, height: 200)
+                    .contextMenu {
+                        ShareLink(item: Image(uiImage: qrCode), preview: SharePreview("My QR Code", image: Image(uiImage: qrCode)))
+                    }
             }
             .navigationTitle("Your code")
+            .onAppear(perform: updateQRCode)
+            .onChange(of: name, updateQRCode)
+            .onChange(of: emailAddress, updateQRCode)
         }
+    }
+    
+    func updateQRCode() {
+        qrCode = generateQRCode(from: "\(name)\n\(emailAddress)")
     }
     
     func generateQRCode(from string: String) -> UIImage {
@@ -41,7 +54,8 @@ struct MeView: View {
         if let outputImage = filter.outputImage {
             if let cgImage = context.createCGImage(outputImage, from: outputImage.extent)
             {
-                return UIImage(cgImage: cgImage)
+                qrCode = UIImage(cgImage: cgImage)
+                return qrCode
             }
         }
         return UIImage(systemName: "xmark.circle") ?? UIImage()
