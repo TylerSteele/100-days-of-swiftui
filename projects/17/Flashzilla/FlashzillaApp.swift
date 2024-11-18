78055ff34cc300658a94caba2d66651eb2137ec6
SteeleTyler <tylermsteele@outlook.com>
2024-11-18 15:16:14 -0500

    Complete day 88

---
+++ projects/17/Flashzilla/FlashzillaApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlashzillaApp.swift
+//  Flashzilla
+//
+//  Created by Tyler Steele on 11/17/24.
+//
+
+import SwiftUI
+
+@main
+struct FlashzillaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
