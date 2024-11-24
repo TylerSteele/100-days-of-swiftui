15db2eff0b9434660840583527db2c1ed867c1d3
SteeleTyler <tylermsteele@outlook.com>
2024-11-24 13:26:03 -0500

    Complete day 98

---
+++ projects/19/SnowSeeker/SnowSeekerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SnowSeekerApp.swift
+//  SnowSeeker
+//
+//  Created by Tyler Steele on 11/23/24.
+//
+
+import SwiftUI
+
+@main
+struct SnowSeekerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
